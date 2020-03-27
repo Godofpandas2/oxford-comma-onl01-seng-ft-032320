eefbbeabd21d71a36fc5ea90dd785423905c00a1
David Hutchison <dhutchis68516@gmail.com>
2020-03-27 19:25:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,11 @@ def oxford_comma(array)
   elsif array.count == 2
     return array.join(" and ")
    else
+<<<<<<< HEAD
     array [-1] = "and #{array [-1]}"
+=======
+    array [-1] << "#{array [-1]}and "
+>>>>>>> 941bd07c2abe7cd7c57556dc03dfe323bd53ad51
     return array.join(", ")
   end
 end
